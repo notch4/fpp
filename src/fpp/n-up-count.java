4bbbf2bc2716da22d9230a07b7b52dedf0101f18
Yubraj Pokharel <yubraj.pokharel555@gmail.com>
2015-10-30 14:51:27 -0500

    Create n-up-count.java

---
+++ src/fpp/n-up-count.java
@@ -0,0 +1,25 @@
+set 1  . Q.No 2
+
+public static int nUpCount(int[] a, int n){
+		
+		int counter = 0;
+		int[] partialSumArray = new int[a.length];
+		int partialSum = 0;
+		
+		for(int i = 0; i<a.length; i++){
+			
+			partialSum += a[i];
+			
+			System.out.println(partialSum);
+			
+			partialSumArray[i] = partialSum;
+			
+			if(partialSum > n && partialSumArray[i-1] <= n){
+				counter++;
+				System.out.println("here");
+			}
+			
+		}
+		
+		return counter;
+	}
